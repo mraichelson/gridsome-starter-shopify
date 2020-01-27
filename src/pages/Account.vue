5bb3e8392e6494887ab2b735f87ef9638e528643
Travis Reynolds <travis@cirruscreative.co.uk>
2020-01-27 16:50:50 +0000

    Split account into components

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -14,76 +14,26 @@
         </div>
       </div>
     </div>
-    <div class="columns">
-      <div class="column is-half">
-        <h4 class="subtitle is-family-secondary">
-          Order history
-        </h4>
-        <div v-if="customer">
-          <p v-if="!orders.length">
-            You have no orders.
-          </p>
-          <table
-            v-else
-            class="table is-fullwidth">
-            <thead>
-              <tr>
-                <th>Order #</th>
-                <th>Total Items</th>
-                <th>Total Price</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="order in orders"
-                :key="order.id">
-                <th>{{ order.name }}</th>
-                <th>{{ order.lineItems.edges.length }}</th>
-                <th>{{ formatCurrency(order.totalPrice) }}</th>
-                <th>
-                  <a
-                    :href="order.statusUrl"
-                    target="_blank"
-                    rel="noreferrer">
-                    View Order
-                  </a>
-                </th>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div class="column is-half">
-        <h4 class="subtitle is-family-secondary">
-          {{ displayName }}
-        </h4>
-        <p>{{ customer.email }}</p>
-        <br>
-        <div v-if="customer.defaultAddress">
-          <p
-            v-for="(line, i) in customer.defaultAddress.formatted"
-            :key="i">
-            {{ line }}
-          </p>
-        </div>
-      </div>
+    <div
+      v-if="customer"
+      class="columns">
+      <account-orders :orders="orders" />
+      <account-details :customer="customer" />
     </div>
   </Layout>
 </template>
 
 <script>
+// Packages
 import gql from 'graphql-tag'
+// Components
+import AccountOrders from '@/components/Account/AccountOrders'
+import AccountDetails from '@/components/Account/AccountDetails'
 export default {
   name: 'Account',
+  components: { AccountOrders, AccountDetails },
   computed: {
-    displayName () { return this.customer.firstName ? `${this.customer.firstName} ${this.customer.lastName}` : this.customer.displayName },
     orders () { return this.customer.orders.edges.map(({ node }) => node) }
-  },
-  methods: {
-    formatCurrency ({ currencyCode, amount }) {
-      return new Intl.NumberFormat('en-GB', { style: 'currency', currency: currencyCode }).format(amount)
-    }
   },
   apollo: {
     customer: {
