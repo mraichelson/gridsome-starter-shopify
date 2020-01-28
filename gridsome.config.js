b68740241b8196e0a5a6489b252a43df1087c972
Travis Reynolds <travis@cirruscreative.co.uk>
2020-01-28 17:00:20 +0000

    Add GuessJS plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteName: 'Gridsome + Shopify 😍',
   siteDescription: 'A full-featured Shopify starter kit for Gridsome.',
   siteUrl: 'https://gridsome-shopify-starter.netlify.com',
+  pathPrefix: '/cheese',
   templates: {
     ShopifyProduct: [
       {
@@ -78,6 +79,16 @@ module.exports = {
       use: '@gridsome/plugin-google-analytics',
       options: {
         id: process.env.GRIDSOME_ANALYTICS_ID
+      }
+    },
+    {
+      use: 'gridsome-plugin-guess-js',
+      options: {
+        viewId: process.env.GUESS_ANALYTICS_VIEW,
+        jwt: {
+          clientEmail: process.env.GUESS_ANALYTICS_EMAIL,
+          privateKey: process.env.GUESS_ANALYTICS_KEY
+        }
       }
     },
     {
